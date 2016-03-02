bd3de4276e54474768141e574bdef3795c252ba7
michallaskowski <michal.laskowski.dev@gmail.com>
2016-03-02 16:10:22 +0100

    Temporary, to check push to hockey

--- fastlane/travis.sh
+++ fastlane/travis.sh
@@ -5,11 +5,10 @@ if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
   exit $?
 fi
 
-if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+#if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
   # Travis fetches a shallow clone. We use commit count until HEAD for build number. In order to assure that the count is correct we have to `unshallow` Travis' clone.
   git fetch --unshallow
 
   fastlane beta
   exit $?
-fi
-
+#fi
